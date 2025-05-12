5a658f204f1cb7e4c6b43574f9af1501cba0bb7a
ciara-gospel <arletteologuie@gmail.com>
2025-05-12 17:11:32 +0100

    configuring the logging system with winston and shutting down

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ import morgan from 'morgan';
 import { fileURLToPath } from 'node:url';
 import path, { dirname } from 'node:path';
 
+import winstonLogger from "./utils/logger.js"
+
 
 import indexRouter from './routes/index.js';
 import usersRouter from './routes/users.js';
@@ -15,7 +17,9 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-app.use(morgan('dev'));
+const morganFormat = process.env.NODE_ENV === "production" ? "combined" : 'dev'
+app.use(morgan(morganFormat, { stream: winstonLogger.stream }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
