8b9bd172e7565e37ec1661acc3044396c54d8984
ciara-gospel <arletteologuie@gmail.com>
2025-05-27 14:25:57 +0100

    update access

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:5174',
     credentials: true,
 }))
 
